7530044784366f6ff37394f612528818b1f05c62
Nishi Gandhi <72174760+nishigandhi1998@users.noreply.github.com>
2022-12-27 16:16:01 +0530

    Update Ollivander_ Inventory.sql

--- Ollivander_ Inventory.sql
+++ Ollivander_ Inventory.sql
@@ -47,6 +47,19 @@ The minimum number of galleons needed for
 */
 
 
+select 
+w.id, 
+wp.age, 
+w.coins_needed, 
+w.power 
+from Wands as w 
+join Wands_Property as wp
+on w.code = wp.code
+where w.coins_needed = (select min(coins_needed)
+                       from Wands w2 inner join Wands_Property wp2 
+                       on w2.code = wp2.code 
+                       where wp2.is_evil = 0 and wp.age = wp2.age and w.power = w2.power)
+order by w.power desc, wp.age desc;
 
 
 
